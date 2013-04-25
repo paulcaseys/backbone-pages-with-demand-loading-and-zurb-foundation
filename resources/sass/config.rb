1786fa2a6d0c80eff2f978a8f481c31d3814552c
paul casey <paulcaseys@gmail.com>
2013-04-25 10:52:13 +1000

    updating to backbone 1.0.0

--- resources/sass/config.rb
+++ resources/sass/config.rb
@@ -3,6 +3,12 @@ dir = File.dirname(__FILE__)
 
 output_style = :compressed #:expanded or :nested or :compact or :compressed
 
+
+# Set this to the root of your project when deployed:
+http_path = ""
+images_dir = "../images"
+relative_assets = true
+
 # Load the sencha-touch framework automatically.
 # load File.join(dir, '..', '..', 'sdk', 'resources', 'themes')
 
